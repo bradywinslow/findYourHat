349295545cecb6a0460750bbbb5597de6b18c942
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-03-23 09:16:27 -0700

    update name of browser title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from '../styles/Header.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPiedPiperHat } from '@fortawesome/free-brands-svg-icons';
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
     return (
         <div className={styles.headerContainer}>
             <div className={styles.headerTitle}>
                 <h1 className={styles.title}>Find Your Hat</h1>
-                <FontAwesomeIcon icon={faPiedPiperHat} className={styles.headerHat}/>
+                <FontAwesomeIcon icon={faHatWizard} className={styles.headerHat}/>
             </div>
             <div className={styles.gameInstructions}>
                 <p>Navigate the field while avoiding the holes to find your hat!</p>
