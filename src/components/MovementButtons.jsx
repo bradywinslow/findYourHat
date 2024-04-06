3295cf0ab68dcfe003aa2f55635fb0d39e5368ca
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-04-06 09:18:47 -0700

    style form, buttons, field to make more user friendly

--- src/components/MovementButtons.jsx
+++ src/components/MovementButtons.jsx
@@ -9,29 +9,26 @@ import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 export default function MovementButtons({ moveUp, moveLeft, moveRight, moveDown, isGameOver }) {
     return (
         <>
-            <div className={styles.moveUpButtonDiv}>
-                <div className={styles.moveUpButtonContainer}>
-                    <button className={styles.moveUpButton} type='button' onClick={moveUp} disabled={isGameOver}>
-                        Move Up<FontAwesomeIcon icon={faArrowUp} className={styles.faArrowUp} />
-                    </button>
-                </div>
-            </div>
-            <div className={styles.moveSidewaysButtonDiv}>
+            <div className={styles.movementButtonsDiv}>
                 <div className={styles.moveLeftButtonContainer}>
                     <button className={styles.moveLeftButton} type='button' onClick={moveLeft} disabled={isGameOver}>
-                        Move Left<FontAwesomeIcon icon={faArrowLeft} className={styles.faArrowLeft} />
+                        Left<FontAwesomeIcon icon={faArrowLeft} className={styles.faArrowLeft} />
+                    </button>
+                </div>
+                
+                <div className={styles.moveUpButtonContainer}>
+                    <button className={styles.moveUpButton} type='button' onClick={moveUp} disabled={isGameOver}>
+                        Up<FontAwesomeIcon icon={faArrowUp} className={styles.faArrowUp} />
+                    </button>
+                </div>
+                <div className={styles.moveDownButtonContainer}>
+                    <button className={styles.moveDownButton} type='button' onClick={moveDown} disabled={isGameOver}>
+                        Down<FontAwesomeIcon icon={faArrowDown} className={styles.faArrowDown} />
                     </button>
                 </div>
                 <div className={styles.moveRightButtonContainer}>
                     <button className={styles.moveRightButton} type='button' onClick={moveRight} disabled={isGameOver}>
-                        Move Right<FontAwesomeIcon icon={faArrowRight} className={styles.faArrowRight} />
-                    </button>
-                </div>
-            </div>
-            <div className={styles.moveDownButtonDiv}>
-                <div className={styles.moveDownButtonContainer}>
-                    <button className={styles.moveDownButton} type='button' onClick={moveDown} disabled={isGameOver}>
-                        Move Down<FontAwesomeIcon icon={faArrowDown} className={styles.faArrowDown} />
+                        Right<FontAwesomeIcon icon={faArrowRight} className={styles.faArrowRight} />
                     </button>
                 </div>
             </div>
