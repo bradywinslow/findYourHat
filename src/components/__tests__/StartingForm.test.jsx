825bf8bece5f3b25920bad6dd8d2cb1e074f739c
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-09-23 22:10:22 -0700

    remove background color from logo and favicons

--- src/components/__tests__/StartingForm.test.jsx
+++ src/components/__tests__/StartingForm.test.jsx
@@ -1,13 +1,44 @@
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom'
 import StartingForm from '../StartingForm.jsx';
+import Game from '../pages/Game.jsx';
+
+function renderInRouter() {
+    const routes = [
+        {
+            path: '/',
+            element: <StartingForm />,
+        },
+        {
+            path: '/game',
+            element: <Game />
+        }
+      ];
+    
+      const router = createMemoryRouter(routes, {
+        initialEntries: ['/'],
+        initialIndex: 0,
+      });
+    
+      render(<RouterProvider router={router} />);
+}
 
 test('clicks button and displays field', async () => {
     // ARRANGE
-    render(<StartingForm />)
+    renderInRouter();
 
     // ACT
+    const width = screen.getByLabelText('Width');
+    userEvent.type(width, '10');
+
+    const height = screen.getByLabelText('Height');
+    userEvent.type(height, '10');
+
+    const percentage = screen.getByLabelText('Percentage');
+    userEvent.type(percentage, '25');
+    
     const playButton = screen.getByRole('button', {
         name: 'Play'
     });
@@ -22,7 +53,7 @@ test('clicks button and displays field', async () => {
 
 test('clicks button and displays movementButtons', async () => {
     // ARRANGE
-    render(<StartingForm />)
+    renderInRouter();
 
     // ACT
     const playButton = screen.getByRole('button', {
