83e4e9325241e9713d912e8f7a72dab7694c0583
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-10-01 22:59:04 -0700

    enter key restarts game when game over modal is open

--- src/components/GameOverModal.jsx
+++ src/components/GameOverModal.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../styles/GameOverModal.module.css';
 
 export default function GameOverModal({ isGameOver, gameOverMessage, restartGame }) {
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === 'Enter') {
+                restartGame();
+            }
+        };
+
+        if (isGameOver) {
+            window.addEventListener('keydown', handleKeyDown);
+        }
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [isGameOver, restartGame]);
+    
     if (!isGameOver) return null;
     
     return (
@@ -9,7 +25,7 @@ export default function GameOverModal({ isGameOver, gameOverMessage, restartGame
             <div className={styles.overlay}>
                 <div className={styles.modal}>
                     <p className={styles.modalText}>{gameOverMessage}</p>
-                    <button onClick={restartGame} className={styles.modalButton}>Play Again</button>
+                    <button onClick={restartGame} onSubmit={restartGame} className={styles.modalButton}>Play Again</button>
                 </div>
             </div>
         </>
