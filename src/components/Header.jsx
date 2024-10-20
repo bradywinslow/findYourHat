33ed5deebe875124b1ddb5a252332f96599e277b
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-10-20 12:07:17 -0700

    make title clickable to nagivate to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from '../styles/Header.module.css';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
+    const navigate = useNavigate();
+    
+    function navigateToHome() {
+        navigate('/');
+        window.location.reload();
+    }
+    
     return (
         <div className={styles.headerContainer}>
             <div className={styles.headerTitle}>
-                <h1 className={styles.title}>Find Your Hat</h1>
+                <h1 className={styles.title} onClick={navigateToHome}>Find Your Hat</h1>
                 <FontAwesomeIcon icon={faHatWizard} className={styles.headerHat}/>
             </div>
             <div className={styles.gameInstructions}>
